13a69bf308a945c086f9b70cae98a34c097df9dc
Silvia <dreamgirlxin@126.com>
2020-06-11 14:15:21 +0800

    把本地工程提交到Git

--- git-test/src/com/silvia/git/GitMain.java
+++ git-test/src/com/silvia/git/GitMain.java
@@ -4,8 +4,4 @@ public class GitMain {
     public static void main(String[] args){
         System.out.println("Hello Git");
     }
-    public void calculate(){
-        int i=0;
-        i+=10;
-    }
 }
