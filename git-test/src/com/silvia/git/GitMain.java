797edaf50a00a895bdf5f8fa6617d5dd9cea3fbb
Silvia <dreamgirlxin@126.com>
2020-06-11 14:14:37 +0800

    把本地工程添加到Git

--- git-test/src/com/silvia/git/GitMain.java
+++ git-test/src/com/silvia/git/GitMain.java
@@ -4,4 +4,8 @@ public class GitMain {
     public static void main(String[] args){
         System.out.println("Hello Git");
     }
+    public void calculate(){
+        int i=0;
+        i+=10;
+    }
 }
